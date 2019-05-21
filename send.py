798756a4f308ba14fb3af41a64a0dc037e758a68
王帅朋 <wsp810@163.com>
2019-05-21 15:52:59 +0800

    [master]feat:无法@某人 尴尬
    1.无法@某人 尴尬

--- send.py
+++ send.py
@@ -6,15 +6,34 @@ import re
 import sys
 import subprocess
 from dingding import DingDing
-
+	
+	
 CAMMAND_GIT_CONFIG_ACCESS_TOKEN = "git config --global pushconfig.accesstoken" #连接gitlab的 token
 
+ding = None
 
 def main():
 	print "main"
-	sendToDingDing("xxx","ttt", "xxx", sender="aaaa", icon= "ss" )
+	setup()
 	
+def setup():
+	global ding
+	if ding:
+		return;
+		
+	access_token = get_access_token()
+	if len(access_token)<=0:
+		print "未设置钉钉群的access_token,无法启用钉钉功能"
+		return
+	
+	ding = DingDing(access_token)
+
 def sendToDingDing(reviewer, mrUrl, msg="", sender="",icon=""):
+	setup()
+	
+	if not ding:
+		print "钉钉初始化失败"
+		return
 	if not reviewer:
 		print "未指定reviewer人,不发送机器人消息"
 		return;	
@@ -25,14 +44,7 @@ def sendToDingDing(reviewer, mrUrl, msg="", sender="",icon=""):
 		print "指定reviewer的人是自己,不发送钉钉消息"
 		return
 	
-	
-	access_token = get_access_token()
-	if len(access_token)<=0:
-		print "未设置钉钉群的access_token,不发送钉钉消息"
-		return
-		
 	print "发送消息给钉钉好友"
-	ding = DingDing(access_token)
 	phone = toMobileWithName(sender)
 	
 	title = sender+"向"+reviewer+"发了一个merge request请求"
@@ -41,15 +53,16 @@ def sendToDingDing(reviewer, mrUrl, msg="", sender="",icon=""):
 	pic_url = icon
 
 	result1 = ding.send_link(title, text, message_url, pic_url)
-	#result2 = ding.send_text(text="点击上边的链接查看",at_mobiles=phone)
-	print result1#,result2
+	print result1
+	textStr = ="点击上边的链接查看@"+reviewer
+	result2 = ding.send_text(text=textStr,at_mobiles=phone)
+	print result2
 
 def get_access_token():
 	return cammand_out_put(CAMMAND_GIT_CONFIG_ACCESS_TOKEN,False,"")
 
 def toMobileWithName(name):
 	return [""]
-
 
 def cammand_out_put(cammand, can_raise, raise_return_value):
 	try:
